47e8071eff1df339b51e27071609518ccdd28186
Emil Segerbäck <emilsegerback@gmail.com>
2018-10-08 18:17:39 +0200

    Add boilerplate for project

---
+++ project/main.js
@@ -0,0 +1,113 @@
+var canvas, gl;
+
+var treeVao, shaderProgram;
+
+let resources = Promise.all(
+    ["shader.vert", "shader.frag"].map(fetchFiles)
+);
+
+function fetchFiles(filename) {
+    return fetch(filename).then(resp => resp.text().then(function(text) {
+        return { filename: filename, contents: text };
+    }));
+}
+
+function render(time) {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(shaderProgram);
+    gl.bindVertexArray(treeVao);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+function createTreeMesh() {
+    let vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    let positionLocation = gl.getAttribLocation(shaderProgram, "a_position");
+    let positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    let vertexPositions = new Float32Array([
+        -0.5, -0.5,
+        0.5, -0.5,
+        0, 0.5
+    ]);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexPositions, gl.STATIC_DRAW);
+
+    gl.enableVertexAttribArray(positionLocation);
+    let size = 2, type = gl.FLOAT, normalize = false, stride = 0, offset = 0;
+    gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
+
+    return vao;
+}
+
+/**
+ * Compiles a shader to be used in a GPU program.
+ * @param {number} type - The type of shader. gl.VERTEX_SHADER/gl.FRAGMENT_SHADER/etc
+ * @param {string} source - The shader source code.
+ * @returns {WebGLShader} The OpenGL shader id of the compiled shader
+ */
+function createShader(type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    // Log an error if the compilation fails
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if (success) {
+        return shader;
+    } else {
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+}
+
+function createProgram(shaderSources) {
+    let shaderTypes = {
+        "shader.vert": gl.VERTEX_SHADER,
+        "shader.frag": gl.FRAGMENT_SHADER
+    };
+
+    var program = gl.createProgram();
+    for (let shaderData of shaderSources) {
+        let shader = createShader(
+            shaderTypes[shaderData.filename],
+            shaderData.contents
+        );
+        gl.attachShader(program, shader);
+    }
+    gl.linkProgram(program);
+
+    // Log an error if the compilation failed
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    } else {
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        return null;
+    }
+}
+
+function init(loadedResources) {
+    shaderProgram = createProgram(loadedResources);
+
+    treeVao = createTreeMesh();
+
+    requestAnimationFrame(render);
+}
+
+function onBodyLoad() {
+    canvas = document.querySelector("#glCanvas");
+    gl = canvas.getContext("webgl2");
+
+    if (gl === null) {
+        alert("Unable to initialize WebGL");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    resources.then(init);
+}
